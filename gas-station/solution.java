822e1c4507e97b8da75b33e31db819ddfeef9f55
Shafique Arnab <shafiquearnab@gmail.com>
2022-01-20 23:02:23 -0500

    Gas Station

---
+++ gas-station/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int gasInTank = 0;
+        int start = 0;
+        
+        // If the total (gas - cost) across the entire route is >= 0, then we know a possible cycle exists
+        int routeGas = 0;
+        
+        for (int i = 0; i < gas.length; i++) {
+            final int delta = gas[i] - cost[i];
+            gasInTank += delta;
+            routeGas += delta;
+            
+            // No gas to get to the next station, means we must be starting from the next station
+            if (gasInTank < 0) {
+                gasInTank = 0;
+                start = i + 1;
+            }
+        }
+        
+        return routeGas >= 0 ? start : -1;
+    }
+}
